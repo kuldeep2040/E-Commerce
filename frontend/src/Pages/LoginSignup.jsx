0c160728014770fe3dcdb438bdcfcdb9740ca704
Kuldeep Singh <83647746+kuldeep2040@users.noreply.github.com>
2024-10-12 08:22:12 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignup = () => {
   const login = async ()=>{
     console.log('login clicked', formData)
     let responceData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://e-commerce-backend-x64f.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/formData',
@@ -39,7 +39,7 @@ const LoginSignup = () => {
   const signup = async ()=>{
     console.log('signup clicked', formData)
     let responceData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://e-commerce-backend-x64f.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/formData',
